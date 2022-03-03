30ce1ab783012a6cb278157e91e2515ae58b25da
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-03-03 22:38:35 +0200

    idoit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = {
     execute: function (message, args) {
-        message.channel.send(new Date(Date.now().toString()))
+        message.channel.send(new Date(Date.now()).toString())
     }
 }
